26b32604169b650b18dd8dca1526f5bfe2f5d347
MeaTLoTioN <ml@erb.pw>
2022-06-23 17:51:21 +0100

    make indent spacing configurable

--- spf_check.sh
+++ spf_check.sh
@@ -10,10 +10,11 @@ esac
 COUNT=0
 DEPTH=0
 mDEPTH=0
+INDENT=2
 checkIncludes() {
   SPF=$1
   for I in $(dig -t txt $SPF +short|grep -oE "include:.* |ip[4,6]:.* "|sed 's/include://g; s/ip[4,6]://g; s/\"//g'|tr ' ' '\n'); do
-    [[ $DEBUG ]] && printf "%*s%s %s\n" $(($DEPTH*2)) '' "$I"
+    [[ $DEBUG ]] && printf "%*s%s %s\n" $(($DEPTH*$INDENT)) '' "$I"
     if [[ ! ${I} =~ ^[0-9] ]]; then
       DEPTH=$(($DEPTH+1))
       mDEPTH=$(($mDEPTH+1))
